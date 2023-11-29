af1eb0ce7c6150fc1d35ef1dd789ad5a0fcca5f7
tristanvela <sopaloca.12@outlook.com>
2023-11-29 01:17:34 -0500

    CarritoCompra

---
+++ ProyectoRestauranteDSW/Controllers/RestauranteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoRestauranteDSW.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        public IActionResult MenuCatalog()
+        {
+            return View();
+        }
+
+        public IActionResult MenuCatalogDetail( int id) 
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmOrderCar() 
+        {
+            return View();
+        }
+    }
+
+}
