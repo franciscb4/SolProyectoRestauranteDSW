2f311b6bb8c3a53b2254f3adb2ccf1486b5becc6
franciscb4 <franciscobulnes04@gmail.com>
2023-11-29 12:27:28 -0500

    migracion

--- ProyectoRestauranteDSW/DataAccess/PlatosEntity.cs
+++ ProyectoRestauranteDSW/DataAccess/PlatosEntity.cs
@@ -9,6 +9,7 @@ namespace ProyectoRestauranteDSW.DataAccess
         public string NombrePlato { get; set; }
         public string Precio { get; set; }
         public string Descripcion { get; set; }
+        public string Imagen { get; set; }
 
     }
 }
---
+++ ProyectoRestauranteDSW/Migrations/20231129172658_MigracionFinal.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoRestauranteDSW.Migrations
+{
+    public partial class MigracionFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreLocal = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    URL = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locales", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NroMesa = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mesas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Platos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombrePlato = table.Column<string>(nullable: true),
+                    Precio = table.Column<string>(nullable: true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Imagen = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellido = table.Column<string>(nullable: true),
+                    DNI = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Correo = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Rol = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaReserva = table.Column<DateTime>(nullable: false),
+                    HoraReserva = table.Column<DateTime>(nullable: false),
+                    Cantidad = table.Column<int>(nullable: false),
+                    MesaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservas_Mesas_MesaId",
+                        column: x => x.MesaId,
+                        principalTable: "Mesas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaVenta = table.Column<DateTime>(nullable: false),
+                    MontoTotal = table.Column<double>(nullable: false),
+                    UsuariosId = table.Column<int>(nullable: true),
+                    MesaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Venta_Mesas_MesaId",
+                        column: x => x.MesaId,
+                        principalTable: "Mesas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Venta_Usuarios_UsuariosId",
+                        column: x => x.UsuariosId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleVenta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(nullable: false),
+                    Total = table.Column<double>(nullable: false),
+                    VentaId = table.Column<int>(nullable: true),
+                    PlatosId = table.Column<int>(nullable: true),
+                    UsuariosId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleVenta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetalleVenta_Platos_PlatosId",
+                        column: x => x.PlatosId,
+                        principalTable: "Platos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DetalleVenta_Usuarios_UsuariosId",
+                        column: x => x.UsuariosId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DetalleVenta_Venta_VentaId",
+                        column: x => x.VentaId,
+                        principalTable: "Venta",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleVenta_PlatosId",
+                table: "DetalleVenta",
+                column: "PlatosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleVenta_UsuariosId",
+                table: "DetalleVenta",
+                column: "UsuariosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleVenta_VentaId",
+                table: "DetalleVenta",
+                column: "VentaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_MesaId",
+                table: "Reservas",
+                column: "MesaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venta_MesaId",
+                table: "Venta",
+                column: "MesaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venta_UsuariosId",
+                table: "Venta",
+                column: "UsuariosId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetalleVenta");
+
+            migrationBuilder.DropTable(
+                name: "Locales");
+
+            migrationBuilder.DropTable(
+                name: "Reservas");
+
+            migrationBuilder.DropTable(
+                name: "Platos");
+
+            migrationBuilder.DropTable(
+                name: "Venta");
+
+            migrationBuilder.DropTable(
+                name: "Mesas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
