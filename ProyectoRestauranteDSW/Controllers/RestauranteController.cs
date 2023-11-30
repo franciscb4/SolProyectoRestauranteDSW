307e6c92c301cd47f0d4c538cabfb568d0d81ad2
tristanvela <sopaloca.12@outlook.com>
2023-11-30 02:58:18 -0500

    CarritoV2

--- ProyectoRestauranteDSW/Controllers/RestauranteController.cs
+++ ProyectoRestauranteDSW/Controllers/RestauranteController.cs
@@ -1,18 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoRestauranteDSW.DataAccess;
+using ProyectoRestauranteDSW.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProyectoRestauranteDSW.Controllers
 {
     public class RestauranteController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly RestauranteContext _restauranteContext;
+
+        public RestauranteController(
+            RestauranteContext restauranteContext,
+            IMapper mapper)
+        {
+            _restauranteContext = restauranteContext;
+            _mapper = mapper;
+        }
         public IActionResult MenuCatalog()
         {
-            return View();
+            var platoListBD = _restauranteContext.Plato.ToList();
+            var menuModelList = _mapper.Map<List<PlatosModel>>(platoListBD);
+            return View(menuModelList);
         }
 
-        public IActionResult MenuCatalogDetail( int id) 
+        public IActionResult MenuCatalogDetail(int id)
         {
-            return View();
+            var platoById = _restauranteContext.Plato.SingleOrDefault(p => p.Id == id);
+
+            if (platoById == null)
+            {
+                
+                return NotFound();
+            }
+
+            var platoView = _mapper.Map<PlatosModel>(platoById);
+            return View(platoView);
         }
+
 
         public IActionResult ConfirmOrderCar() 
         {
---
+++ ProyectoRestauranteDSW/Models/PlatoTemporalModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoRestauranteDSW.Models
+{
+    public class PlatoTemporalModel
+    {
+        public int Id { get; set; }
+        public string PlatoName { get; set; }
+        public double PlatoPrice { get; set; }
+        public string Descripcion { get; set; }
+        public string Img { get; set; }
+    }
+}
